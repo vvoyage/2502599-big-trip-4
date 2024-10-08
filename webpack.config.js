63a8d7a1e9e88c9c54fa784e91f99bea92bd790b
Voyage <kirillbatalovwork@mail.ru>
2024-10-09 00:19:01 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,66 @@
 
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin'); 
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
- entry: './src/main.js', // Точка входа - main.js
- output: {
-  filename: 'bundle.js', // Имя файла сборки - bundle.js
-  path: path.resolve(__dirname, 'build'), // Абсолютный путь к директории сборки
- },
- plugins: [
-    new CopyWebpackPlugin({
-     patterns: [
-      { from: 'public', to: '' }, // Скопируем все из папки "public" в корень "build"
-     ],
+  entry: './src/main.js', // Точка входа для сборки проекта
+  output: {
+    filename: 'bundle.[contenthash].js', // Имя выходного файла сборки
+    path: path.resolve(__dirname, 'build'), // Путь для выходного файла сборки
+    clean: {
+        // сохранение файлов
+        keep: /\ignored\/dir\//
+      }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
+        use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // применять загрузчик только к файлам .js
+        exclude: /node_modules/, // не применять загрузчик к файлам в папке node_modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map', // Здесь задаем создание карт 
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: { sourceMaps: true } // Командуем Babel создавать карты 
+      }
+    }]},
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      ignore: ['index.html']
     }),
-   ],
- devtool: 'source-map', // Активируем генерацию source-maps
+    new CopyWebpackPlugin({
+      patterns: [
+      {
+        from: path.resolve(__dirname, './src/public'),
+        to: path.resolve(__dirname, './src/build')
+      }
+    ]})
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Каталог для статики
+    },
+    open: true, // Автоматически открывать браузер
+  },
+  mode: 'development', // Режим сборки
 };
-
-
