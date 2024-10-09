a1327913d45089865fa5517cbcd877193fbf00ed
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 04:08:05 +0500

    Update filters-presenter.js

--- src/presenter/filters-presenter.js
+++ src/presenter/filters-presenter.js
@@ -1,14 +1,30 @@
 import {
   render
 } from '../framework/render.js';
-
 import FiltersView from '../view/filters-view.js';
+import { FilterSettings } from '../const.js';
+import { filterByType } from '../utils';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 
 export default class FiltersPresenter {
+  #pointsModel = null;
+  #filters = [];
+
+  constructor({pointsModel}) {
+    this.#pointsModel = pointsModel;
+
+    this.#filters = Object.entries(filterByType)
+      .map(([type, filter]) => ({
+        ...FilterSettings[type],
+        type,
+        disabled: filter(this.#pointsModel.get()).length === 0
+      }));
+  }
 
   init() {
-    render(new FiltersView(), filtersContainer);
+    render(new FiltersView({
+      items: this.#filters
+    }), filtersContainer);
   }
 }
