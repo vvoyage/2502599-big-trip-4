7683bb56b934f9fabc2623451db72ab3efe4a84d
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 02:47:45 +0500

    Update board-presenter.js

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -6,25 +6,37 @@ import EventItemView from '../view/item.js';
 import PointEditorView from '../view/editor-view.js';
 import SortingView from '../view/sort.js';
 
-const POINTS_COUNT = 3;
-
 export default class PointsPresenter {
   listComponent = new EventListView();
   sortingComponent = new SortingView();
 
   constructor({
-    tripEventsContainer
+    tripEventsContainer,
+    pointsModel,
+    offersModel,
+    destinationsModel
   }) {
     this.tripEventsContainer = tripEventsContainer;
+    this.pointsModel = pointsModel;
+    this.offersModel = offersModel;
+    this.destinationsModel = destinationsModel;
   }
 
   init() {
+    this.eventPoints = [...this.pointsModel.getEventPoints()];
+
     render(this.sortingComponent, this.tripEventsContainer);
     render(this.listComponent, this.tripEventsContainer);
     render(new PointEditorView(), this.listComponent.getElement());
 
-    for (let i = 0; i < POINTS_COUNT; i++) {
-      render(new EventItemView(), this.listComponent.getElement());
+    for (let i = 0; i < this.eventPoints.length; i++) {
+      const offers = this.offersModel.getByType(this.eventPoints[i].type);
+      const destination = this.destinationsModel.getById(this.eventPoints[i].destination);
+      render(new EventItemView({
+        eventPoint: this.eventPoints[i],
+        offers,
+        destination
+      }), this.listComponent.getElement());
     }
   }
 }
