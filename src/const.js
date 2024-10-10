12174044b41d0fc16c3b1c2dc7d5d969558102f0
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 13:18:19 +0500

    Update const.js

--- src/const.js
+++ src/const.js
@@ -1,10 +1,14 @@
-const POINTS_COUNT = 5;
 const MSEC_IN_SEC = 1000;
 const SEC_IN_MIN = 60;
 const MIN_IN_HOUR = 60;
 const HOUR_IN_DAY = 24;
+const MSEC_IN_HOUR = MSEC_IN_SEC * SEC_IN_MIN * MIN_IN_HOUR;
+const MSEC_IN_DAY = MSEC_IN_HOUR * HOUR_IN_DAY;
+
+const POINTS_COUNT = 5;
 const MAX_PRICE_VALUE = 200;
 const MAX_IMAGES_COUNT = 5;
+
 const EVENT_TYPES = [
   'taxi',
   'bus',
@@ -32,6 +36,11 @@ const DESCRIPTIONS = [
   'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, dolorem.',
   'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit ad eaque cupiditate praesentium maxime.',
 ];
+
+const PointMode = {
+  IDLE: 'IDLE',
+  EDITABLE: 'EDITABLE',
+};
 
 const FilterType = {
   ANY: 'any',
@@ -110,9 +119,6 @@ const MocksMaxCount = {
   POINTS: 5,
 };
 
-const MSEC_IN_HOUR = MSEC_IN_SEC * SEC_IN_MIN * MIN_IN_HOUR;
-const MSEC_IN_DAY = MSEC_IN_HOUR * HOUR_IN_DAY;
-
 export {
   POINTS_COUNT,
   EVENT_TYPES,
@@ -121,6 +127,7 @@ export {
   CITIES,
   DESCRIPTIONS,
   SORTING_COLUMNS,
+  PointMode,
   FilterType,
   FilterSettings,
   SortType,
