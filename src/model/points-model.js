c7f19a9c0b83fa45f31a4cd5d1af89aa42ef0d98
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 18:14:18 +0500

    Update points-model.js

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,14 +1,27 @@
-export default class PointsModel {
+import Observable from '../framework/observable.js';
+
+export default class PointsModel extends Observable {
   #service = null;
   #points = null;
 
   constructor(service) {
+    super();
     this.#service = service;
     this.#points = this.#service.points;
   }
 
   get() {
     return this.#points;
+  }
+
+  add(point) {
+    this.#points.push(point);
+    this._notify(point);
+  }
+
+  delete(point) {
+    this.#points = this.#points.filter((item) => item.id !== point.id);
+    this._notify(point);
   }
 }
 
