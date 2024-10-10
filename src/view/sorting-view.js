7f9e34b471fc8cb38c4fa41b7f4ad86d93f61591
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 19:20:04 +0500

    Update sorting-view.js

--- src/view/sorting-view.js
+++ src/view/sorting-view.js
@@ -1,37 +1,41 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-function createSortingItemTemplate(column) {
+function createSortingItemTemplate(column, selectedSortType) {
   const { type, label, active, defaultSelected } = column;
+  const isChecked = selectedSortType === type || defaultSelected;
   return `
     <div class="trip-sort__item  trip-sort__item--${type}">
-      <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="${type}" ${!active ? 'disabled' : ''} ${defaultSelected ? 'checked' : ''}>
+      <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="${type}" ${!active ? 'disabled' : ''} ${isChecked ? 'checked' : ''}>
       <label class="trip-sort__btn" for="sort-${type}">${label}</label>
     </div>`;
 }
 
-function createSortingTemplate(items) {
+function createSortingTemplate(items, selectedSortType) {
   return `<form class="trip-events__trip-sort trip-sort" action="#" method="get">
-    ${items.map(createSortingItemTemplate).join('')}
+    ${items.map((el) => createSortingItemTemplate(el, selectedSortType)).join('')}
   </form>`;
 }
 
 export default class SortingView extends AbstractView {
   #items = [];
+  #selectedSortType = null;
   #onSortChange = null;
 
   constructor({
     items,
+    selectedSortType,
     onSortChange
   }) {
     super();
 
     this.#items = items;
+    this.#selectedSortType = selectedSortType;
     this.#onSortChange = onSortChange;
     this.element.addEventListener('change', this.#sortingChangeHandler);
   }
 
   get template() {
-    return createSortingTemplate(this.#items);
+    return createSortingTemplate(this.#items, this.#selectedSortType);
   }
 
   #sortingChangeHandler = (evt) => {
