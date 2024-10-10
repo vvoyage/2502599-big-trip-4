1cd5b8d90872806812000cf42842ef67e1a31760
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 19:19:41 +0500

    Update offers-model.js

--- src/model/offers-model.js
+++ src/model/offers-model.js
@@ -1,8 +1,12 @@
-export default class OffersModel {
+import Observable from '../framework/observable.js';
+import { deleteItem, updateItem } from '../utils.js';
+
+export default class OffersModel extends Observable {
   #service = null;
   #offers = null;
 
   constructor(service) {
+    super();
     this.#service = service;
     this.#offers = this.#service.offers;
   }
@@ -13,5 +17,20 @@ export default class OffersModel {
 
   getByType(type) {
     return this.#offers.find((offers) => offers.type === type.toLowerCase()).offers;
+  }
+
+  add(type, offer) {
+    this.#offers.push(offer);
+    this._notify(type, offer);
+  }
+
+  update(type, offer) {
+    this.#offers = updateItem(this.#offers, offer);
+    this._notify(type, offer);
+  }
+
+  delete(type, offer) {
+    this.#offers = deleteItem(this.#offers, offer);
+    this._notify(type, offer);
   }
 }
