c080436c8db8d349b1849ce55572d61984a9ae9d
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 19:19:48 +0500

    Update filters-presenter.js

--- src/presenter/filters-presenter.js
+++ src/presenter/filters-presenter.js
@@ -5,14 +5,13 @@ import FiltersView from '../view/filters-view.js';
 import { FilterSettings } from '../const.js';
 import { filterByType } from '../utils';
 
-const filtersContainer = document.querySelector('.trip-controls__filters');
-
-
 export default class FiltersPresenter {
+  #container = null;
   #pointsModel = null;
   #filters = [];
 
-  constructor({pointsModel}) {
+  constructor({ container, pointsModel }) {
+    this.#container = container;
     this.#pointsModel = pointsModel;
 
     this.#filters = Object.entries(filterByType)
@@ -23,9 +22,11 @@ export default class FiltersPresenter {
       }));
   }
 
+
   init() {
     render(new FiltersView({
       items: this.#filters
-    }), filtersContainer);
+    }), this.#container);
   }
+
 }
