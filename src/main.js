0bab7977fdff1fff441aa82578e0feb71c81201a
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 18:14:15 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,24 +5,37 @@ import RoutePresenter from './presenter/route-presenter.js';
 import FiltersPresenter from './presenter/filters-presenter.js';
 import TripInfoPresenter from './presenter/trip-info-presenter.js';
 import MockService from './service/mock-service.js';
+import CreatePointPresenter from './presenter/create-point-presenter.js';
 
 const mockService = new MockService();
 const pointsModel = new PointsModel(mockService);
 const offersModel = new OffersModel(mockService);
 const destinationsModel = new DestinationsModel(mockService);
 
-const container = document.querySelector('.trip-events');
+const pointsContainer = document.querySelector('.trip-events');
+const filtersContainer = document.querySelector('.trip-controls__filters');
+const tripMainContainer = document.querySelector('.trip-main');
+
+const createPointPresenter = new CreatePointPresenter({
+  container: tripMainContainer,
+  editorContainer: pointsContainer,
+  pointsModel,
+  offersModel,
+  destinationsModel,
+});
 
 const routePresenter = new RoutePresenter({
-  container,
+  container: pointsContainer,
+  createPointBtnContainer: tripMainContainer,
   pointsModel,
   offersModel,
   destinationsModel
 });
 
-const filtersPresenter = new FiltersPresenter({ pointsModel });
-const tripInfoPresenter = new TripInfoPresenter();
+const filtersPresenter = new FiltersPresenter({ container: filtersContainer, pointsModel });
+const tripInfoPresenter = new TripInfoPresenter(tripMainContainer);
 
+createPointPresenter.init();
 routePresenter.init();
 filtersPresenter.init();
 tripInfoPresenter.init();
