606f43fcb9ec4d8961eaab9ada0b6703df8156dc
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 19:19:39 +0500

    Update destinations-model.js

--- src/model/destinations-model.js
+++ src/model/destinations-model.js
@@ -1,8 +1,12 @@
-export default class DestinationsModel {
+import Observable from '../framework/observable.js';
+import { deleteItem, updateItem } from '../utils.js';
+
+export default class DestinationsModel extends Observable {
   #service = null;
   #destinations = null;
 
   constructor(service) {
+    super();
     this.#service = service;
     this.#destinations = this.#service.destinations;
   }
@@ -13,5 +17,20 @@ export default class DestinationsModel {
 
   getById(id) {
     return this.#destinations.find((destination) => destination.id === id);
+  }
+
+  add(type, destination) {
+    this.#destinations.push(type, destination);
+    this._notify(destination);
+  }
+
+  update(type, destination) {
+    this.#destinations = updateItem(this.#destinations, destination);
+    this._notify(type, destination);
+  }
+
+  delete(type, destination) {
+    this.#destinations = deleteItem(this.#destinations, destination);
+    this._notify(type, destination);
   }
 }
