16e0480c68c16980a3ea2766d606663a24710028
Voyage <kirillbatalovwork@mail.ru>
2024-10-10 14:02:01 +0500

    Update route-presenter.js

--- src/presenter/route-presenter.js
+++ src/presenter/route-presenter.js
@@ -30,9 +30,7 @@ export default class RoutePresenter {
     this.#pointsModel = pointsModel;
     this.#offersModel = offersModel;
     this.#destinationsModel = destinationsModel;
-    this.#points = sortByType[this.#currentSortType]([
-      ...this.#pointsModel.get(),
-    ]);
+    this.#points = sortByType[this.#currentSortType]([...this.#pointsModel.get()]);
   }
 
   init() {
