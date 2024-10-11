c0236128fc78cadcd51350d8312498ffdf5a892e
Voyage <kirillbatalovwork@mail.ru>
2024-10-11 18:26:20 +0500

    Update route-presenter.js

--- src/presenter/route-presenter.js
+++ src/presenter/route-presenter.js
@@ -4,40 +4,63 @@ import {
 } from '../framework/render.js';
 import EventsListView from '../view/events-list-view.js';
 import EventsListEmptyView from '../view/events-list-empty-view.js';
-import { SORTING_COLUMNS, SortType, UpdateType } from '../const.js';
+import { SORTING_COLUMNS, SortType, UiBlockerLimit, UpdateType, UserAction } from '../const.js';
 import PointPresenter from './point-presenter.js';
-import { sortByType } from '../utils.js';
+import { filterByType, sortByType } from '../utils.js';
 import SortingView from '../view/sorting-view.js';
+import LoaderView from '../view/loader-view.js';
+import UiBlocker from '../framework/ui-blocker/ui-blocker.js';
+
 
 export default class RoutePresenter {
   #container = null;
+  #createPointPresenter = null;
   #pointsModel = null;
   #destinationsModel = null;
   #offersModel = null;
+  #filtersModel = null;
   #currentSortType = SortType.DAY;
+  #loaderComponent = new LoaderView();
   #listComponent = new EventsListView();
+  #uiBlocker = new UiBlocker({
+    lowerLimit: UiBlockerLimit.LOWER,
+    upperLimit: UiBlockerLimit.UPPER,
+  });
+
   #sortingComponent = null;
   #pointsPresenters = new Map();
+  #isLoading = true;
+  #isError = false;
 
   constructor({
     container,
+    createPointPresenter,
     pointsModel,
     offersModel,
     destinationsModel,
+    filtersModel,
   }) {
     this.#container = container;
+    this.#createPointPresenter = createPointPresenter;
     this.#pointsModel = pointsModel;
     this.#offersModel = offersModel;
     this.#destinationsModel = destinationsModel;
+    this.#filtersModel = filtersModel;
 
-    this.#pointsModel.addObserver(this.#modelEventHandler);
+    this.#pointsModel.addObserver(this.#pointsModelEventHandler);
+    this.#filtersModel.addObserver(this.#filterModelEventHandler);
   }
 
   get points() {
-    return sortByType[this.#currentSortType]([...this.#pointsModel.get()]);
+    const points = this.#pointsModel.get();
+    const filterType = this.#filtersModel.get();
+    const filteredPoints = filterByType[filterType](points);
+
+    return sortByType[this.#currentSortType](filteredPoints);
   }
 
   init() {
+    this.#createPointPresenter.init(this.#userActionHandler);
     this.#renderRoute();
   }
 
@@ -45,6 +68,10 @@ export default class RoutePresenter {
     remove(this.#sortingComponent);
     remove(this.#listComponent);
     this.#clearRoute();
+  }
+
+  #renderLoader() {
+    render(this.#loaderComponent, this.#container);
   }
 
   #renderStub() {
@@ -62,6 +89,13 @@ export default class RoutePresenter {
   }
 
   #renderRoute() {
+    if (this.#isLoading) {
+      this.#createPointPresenter.setButtonDisabled(true);
+      this.#renderLoader();
+      return;
+    }
+
+    this.#createPointPresenter.setButtonDisabled(false);
     if (!this.points.length) {
       this.#renderStub();
       return;
@@ -90,8 +124,7 @@ export default class RoutePresenter {
       container: this.#listComponent.element,
       destinationsModel: this.#destinationsModel,
       offersModel: this.#offersModel,
-      onPointChange: this.#pointChangeHandler,
-      onPointDelete: this.#pointDeleteHandler,
+      onUserAction: this.#userActionHandler,
       onEditorOpen: this.#pointEditHandler,
     });
 
@@ -99,16 +132,48 @@ export default class RoutePresenter {
     this.#pointsPresenters.set(point.id, pointPresenter);
   }
 
+  #userActionHandler = async (actionType, data) => {
+    this.#uiBlocker.block();
+    let presenter;
+    try {
+      switch (actionType) {
+        case UserAction.CREATE_POINT:
+          presenter = this.#createPointPresenter;
+          await this.#createPointHandler(data);
+          break;
+
+        case UserAction.UPDATE_POINT:
+          presenter = this.#pointsPresenters.get(data.id);
+          await this.#updatePointHandler(data);
+          break;
+
+        case UserAction.DELETE_POINT:
+          presenter = this.#pointsPresenters.get(data.id);
+          await this.#deletePointHandler(data);
+          break;
+        default:
+          throw new Error(`RoutePresenter - unknown user action: ${actionType}`);
+      }
+      presenter.resetView();
+    } catch (err) {
+      presenter.triggerError();
+      this.#isError = true;
+    }
+
+    this.#uiBlocker.unblock();
+  };
+
+  #createPointHandler = (data) =>
+    this.#pointsModel.create(UpdateType.MAJOR, data);
+
+  #updatePointHandler = (data) =>
+    this.#pointsModel.update(UpdateType.MINOR, data);
+
+  #deletePointHandler = (data) =>
+    this.#pointsModel.delete(UpdateType.MAJOR, data);
+
   #pointEditHandler = () => {
     this.#pointsPresenters.forEach((presenter) => presenter.resetView());
-  };
-
-  #pointChangeHandler = (updatedPoint) => {
-    this.#pointsModel.update(UpdateType.MINOR, updatedPoint);
-  };
-
-  #pointDeleteHandler = (deletedPoint) => {
-    this.#pointsModel.delete(UpdateType.MAJOR, deletedPoint);
   };
 
   #sortChangeHandler = (sortType) => {
@@ -118,12 +183,35 @@ export default class RoutePresenter {
     this.#renderRoute();
   };
 
-  #modelEventHandler = (type, data) => {
+  #pointsModelEventHandler = (type, data) => {
+    if (this.#isError || data?.error) {
+      return;
+    }
+
     switch (type) {
+      case UpdateType.INIT:
+        this.#isLoading = false;
+        remove(this.#loaderComponent);
+        this.#renderRoute();
+        break;
+
       case UpdateType.PATCH:
         this.#pointsPresenters.get(data.id)?.init(data);
         break;
 
+      case UpdateType.MINOR:
+      case UpdateType.MAJOR:
+      default:
+        this.#clearRoute();
+        this.#renderRoute();
+        break;
+    }
+  };
+
+  #filterModelEventHandler = (type) => {
+    switch (type) {
+      case UpdateType.INIT:
+      case UpdateType.PATCH:
       case UpdateType.MINOR:
       case UpdateType.MAJOR:
       default:
