7f3d8373c6f0f20cb8619969371b19d1b6fbcb88
Voyage <kirillbatalovwork@mail.ru>
2024-10-11 18:26:26 +0500

    Update create-point-button-view.js

--- src/view/create-point-button-view.js
+++ src/view/create-point-button-view.js
@@ -1,22 +1,27 @@
-import AbstractView from '../framework/view/abstract-view.js';
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
 
-const createCreatePointButtonTemplate = () => '<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>';
+const createCreatePointButtonTemplate = (disabled) => `<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button" ${disabled ? 'disabled' : ''}>New event</button>`;
 
-export default class CreatePointButtonView extends AbstractView {
+export default class CreatePointButtonView extends AbstractStatefulView {
   #handleButtonClick = null;
 
-  constructor({ onClick }) {
+  constructor({ onClick, disabled = false}) {
     super();
     this.#handleButtonClick = onClick;
+    this._setState({ disabled });
+    this._restoreHandlers();
+  }
+
+  _restoreHandlers() {
     this.element.addEventListener('click', this.#buttonClickHandler);
   }
 
   get template() {
-    return createCreatePointButtonTemplate();
+    return createCreatePointButtonTemplate(this._state.disabled);
   }
 
   setDisabled(isDisabled) {
-    this.element.disabled = isDisabled;
+    this.updateElement({ disabled: isDisabled });
   }
 
   #buttonClickHandler = (evt) => {
