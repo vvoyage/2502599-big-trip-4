7c713a2ab70aff06c7844175cb903b7ef2ae5639
Voyage <kirillbatalovwork@mail.ru>
2024-10-11 18:26:33 +0500

    Update trip-info-view.js

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -1,22 +1,62 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import {
+  getRouteLabel,
+  getDurationPeriod,
+  getTotalPointsCost,
+} from '../utils.js';
 
-function createTripInfoTemplate() {
-  return (
-    `<section class="trip-main__trip-info  trip-info">
-      <div class="trip-info__main">
-        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
-        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
-      </div>
-      <p class="trip-info__cost">
-        Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
-      </p>
-    </section>`
-  );
-}
+const createTripInfoDummyTemplate = (isLoading) => `
+<section class="trip-main__trip-info  trip-info">
+    <div class="trip-info__main">
+      <h1 class="trip-info__title">${isLoading ? 'Loading...' : 'No points'}</h1>
+      <p class="trip-info__dates">${isLoading ? 'Loading...' : 'Not selected'}</p>
+    </div>
 
+    <p class="trip-info__cost">
+      ${isLoading ? 'Loading...' : 'Total: &euro;&nbsp;<span class="trip-info__cost-value">0</span>'}
+    </p>
+  </section>`;
+
+const createTripInfoTemplate = ({isEmpty, isLoading, routeLabel, periodLabel, cost}) => isEmpty
+  ? createTripInfoDummyTemplate(isLoading)
+  : `<section class="trip-main__trip-info trip-info">
+    <div class="trip-info__main">
+      <h1 class="trip-info__title">${routeLabel}</h1>
+      <p class="trip-info__dates">${periodLabel}</p>
+    </div>
+
+    <p class="trip-info__cost">
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">${cost}</span>
+    </p>
+  </section>`;
 
 export default class TripInfoView extends AbstractView {
+  #points = [];
+  #destinations = null;
+  #offers = null;
+  #isLoading = false;
+
+  constructor({
+    destinations,
+    offers,
+    points,
+    isLoading,
+  }) {
+    super();
+    this.#points = points;
+    this.#destinations = destinations;
+    this.#offers = offers;
+    this.#isLoading = isLoading;
+  }
+
   get template() {
-    return createTripInfoTemplate();
+    const isEmpty = this.#points.length === 0;
+    return createTripInfoTemplate({
+      isEmpty,
+      isLoading: this.#isLoading,
+      routeLabel: isEmpty ? null : getRouteLabel(this.#points, this.#destinations),
+      periodLabel: isEmpty ? null : getDurationPeriod(this.#points),
+      cost: isEmpty ? null : getTotalPointsCost(this.#points, this.#offers),
+    });
   }
 }
