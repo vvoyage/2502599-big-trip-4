69c67e3bd19001acf41019ab41ed692a2529c040
Voyage <kirillbatalovwork@mail.ru>
2024-10-11 18:26:28 +0500

    Update filters-view.js

--- src/view/filters-view.js
+++ src/view/filters-view.js
@@ -1,10 +1,10 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 function createFilterItemTemplate(filter) {
-  const { type, label, defaultSelected, disabled } = filter;
+  const { type, label, selected, disabled } = filter;
   return (
     `<div class="trip-filters__filter">
-      <input id="filter-${type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}" ${defaultSelected ? 'checked' : ''} ${disabled ? 'disabled' : ''}>
+      <input id="filter-${type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}" ${selected ? 'checked' : ''} ${disabled ? 'disabled' : ''}>
       <label class="trip-filters__filter-label" for="filter-${type}">${label}</label>
     </div>`
   );
@@ -32,10 +32,15 @@ export default class FiltersView extends AbstractView {
 
     this.#items = items;
     this.#onFilterChange = onFilterChange;
-    this.element.addEventListener('change', this.#onFilterChange);
+    this.element.addEventListener('change', this.#handleFilterChange);
   }
 
   get template() {
     return createFiltersTemplate(this.#items);
   }
+
+  #handleFilterChange = (evt) => {
+    evt.preventDefault();
+    this.#onFilterChange(evt.target.value);
+  };
 }
